54477f74acd3cbfff918f1dc1234915fe1458374
adamgibbons <adam.d.gibbons@gmail.com>
2013-10-07 22:22:02 -0700

    builds a binary clock. because the world needs it.

---
+++ scripts/binaryClockApp.js
@@ -0,0 +1,62 @@
+var myApp = angular.module('binaryClockApp',[]);
+
+function ClockCtrl($scope) {
+    $scope.seconds = 0;
+    $scope.date = function() {
+        return new Date();
+    };
+}
+
+function HoursCtrl($scope) {
+    $scope.getHours = function() {
+        return $scope.date().getHours();
+    }
+    setInterval(function(){
+        $scope.$apply(function() {
+            $scope.hours = $scope.getHours();
+        });
+    }, 1000);
+    $scope.isLitHours = function(position) {
+        if ($scope.getHours() % position >= (position / 2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }    
+}
+
+function MinutesCtrl($scope) { 
+    $scope.getMinutes = function() {
+        return $scope.date().getMinutes();
+    }
+    setInterval(function(){
+        $scope.$apply(function() {
+            $scope.minutes = $scope.getMinutes();
+        });
+    }, 1000);
+    $scope.isLitMinutes = function(position) {
+        if ($scope.getMinutes() % position >= (position / 2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function SecondsCtrl($scope) {
+    $scope.getSeconds = function() {
+        return $scope.date().getSeconds();
+    }
+    setInterval(function(){
+        $scope.$apply(function() {
+            $scope.seconds = $scope.getSeconds();
+        });
+    }, 1000);
+    $scope.isLitSeconds = function(position) {
+        if ($scope.getSeconds() % position >= (position / 2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
