45b5cad869a27fe535fb8a2b1f42ac5820c86191
adamgibbons <adam.d.gibbons@gmail.com>
2014-02-26 23:51:35 -0800

    adds color schemes

--- scripts/binaryClockApp.js
+++ scripts/binaryClockApp.js
@@ -1,18 +1,20 @@
 var myApp = angular.module('binaryClockApp',[]);
 
 function ClockCtrl($scope) {
+    $scope.colorScheme = 'charcoal';
+
     $scope.date = function() {
         return new Date();
     };
     $scope.setColorScheme = function(colorScheme) {
         $scope.colorScheme = colorScheme;
-    }
+    };
 }
 
 function HoursCtrl($scope) {
     $scope.getHours = function() {
         return $scope.date().getHours();
-    }
+    };
     setInterval(function(){
         $scope.$apply(function() {
             $scope.hours = $scope.getHours();
@@ -23,7 +25,7 @@ function HoursCtrl($scope) {
 function MinutesCtrl($scope) { 
     $scope.getMinutes = function() {
         return $scope.date().getMinutes();
-    }
+    };
     setInterval(function(){
         $scope.$apply(function() {
             $scope.minutes = $scope.getMinutes();
@@ -34,7 +36,7 @@ function MinutesCtrl($scope) {
 function SecondsCtrl($scope) {
     $scope.getSeconds = function() {
         return $scope.date().getSeconds();
-    }
+    };
     setInterval(function(){
         $scope.$apply(function() {
             $scope.seconds = $scope.getSeconds();
